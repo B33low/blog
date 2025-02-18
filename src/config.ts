e2b5ec5ceb16df4b44aa9a2f9c51ed84983f1161
Thomas Bolteau <thomas.bolteau50@gmail.com>
2025-02-18 16:45:05 +0000

    Base

---
+++ src/config.ts
@@ -0,0 +1,94 @@
+
+
+export const SITE_FAVICON = "/favicon.png";
+export const SITE_LOGO = "/leo.png";
+export const SITE_TITLE = "Thomas Bolteau";
+export const SITE_URL = "https://astro-theme-leo.muji.dev";
+export const SITE_DESCRIPTION =
+  `
+  Personal website of Thomas Bolteau.
+`;
+export const ME_AVATAR = "/leo.png";
+export const LANGUAGES = ['en', 'fr'];
+
+export const MENUS = [
+  {
+    title: "About Me",
+    link: "/me",
+    target: "_self",
+  },
+  {
+    title: "Posts",
+    link: "/en/posts",
+    target: "_self",
+  },
+  {
+    title: "Github",
+    link: "https://github.com/Ery4z",
+    target: "_blank",
+    icon: "github",
+  },
+];
+
+export const FOOTER_CONTENT = "Thomas Bolteau. All rights reserved.";
+
+export const CONTACTS = {
+  linkedIn: "https://www.linkedin.com/in/thomas-bolteau",
+  github: "https://github.com/Ery4z",
+  email: "mailto:thomas.bolteau50@gmail.com",
+};
+
+export const SLOGAN = "To infinity and beyond!";
+export const ME_DESCRIPTION =
+  `Hi, I’m Thomas!`;
+
+
+export const WORK_EXPERIENCE = [
+  {
+    company: "ENGIE GEMS",
+    position: "VIE - Software Engineer",
+    startDate: "2023",
+    endDate: "2025",
+  },
+  {
+    company: "NxP Semiconductors",
+    position: "DevOps Intern",
+    startDate: "2021",
+    endDate: "2021",
+  },
+  
+  {
+    company: "Université Catholique de Louvain",
+    position: "Electrical Engineering Student",
+    startDate: "2021",
+    endDate: "2023",
+  },
+  
+  {
+    company: "Centrale Nantes",
+    position: "General Engineering Student",
+    startDate: "2019",
+    endDate: "2023",
+  },
+  
+
+  
+];
+
+
+export const PROJECTS = [
+    // {
+    //   id: "my-first-rocket-launch",
+    //   title: "My First Rocket Launch",
+    //   description: "This is the first rocket launch I participated in. I was a member of the Interstellar Academy Program, and I was responsible for the rocket's navigation system. I was able to successfully navigate the rocket to the target destination.",
+    //   image: 'https://images.unsplash.com/photo-1516849841032-87cbac4d88f7?q=80&w=2370&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    //   externalLink: "https://www.nasa.gov/",
+    //   details: [
+    //     "Explaining space exploration concepts through creative illustrations.",
+    //     "Making it easier for aspiring explorers to understand the basics of interstellar travel.",
+    //     "Won an award in the interstellar creativity competition.",
+    //   ],
+    // },
+  ];
+  
+export const GOOGLE_GTAG = "";
